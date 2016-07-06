f6d7c3cb58998fce8a9351da97aa94104851f404
Micah Cooper <mrcooper@ad.oie.uga.edu>
2016-07-06 14:30:06 -0400

    adding method signatures

---
+++ javascript/database.js
@@ -0,0 +1,24 @@
+//takes a string username, string password and a boolean for is the password hashed
+function authenticateUser(var username, var password, var hashed)
+{
+	return true;
+}
+
+function listEvents()
+{
+var events = {event01:[{eventName:"Success Through Failures", eventLocation:"Atlanta"}.{eventName:"Successfully Failing",eventLocation:"Boston"}]};
+	
+	return events;
+}
+
+function getEvent(var eventid)
+{
+	var event = {eventName:"Success Through Failures 2",eventLocation:"Charlotte"};
+	
+	return event;
+}
+
+function registerEvent(var user, var eventid)
+{
+	return true;
+}

1f24d12da8880029493a8bace6daf00d547e96be
Micah Cooper <mrcooper@ad.oie.uga.edu>
2016-07-06 16:19:44 -0400

    adding dynamic event list

--- javascript/database.js
+++ javascript/database.js
@@ -1,24 +1,25 @@
 //takes a string username, string password and a boolean for is the password hashed
-function authenticateUser(var username, var password, var hashed)
+function authenticateUser(username, userpassword, hashed)
 {
 	return true;
 }
 
 function listEvents()
 {
-var events = {event01:[{eventName:"Success Through Failures", eventLocation:"Atlanta"}.{eventName:"Successfully Failing",eventLocation:"Boston"}]};
+	var events = {event01:[{eventName:"Success Through Failures", eventLocation:"Atlanta"},{eventName:"Successfully Failing",eventLocation:"Boston"}]};
 	
+	document.write("hi");
 	return events;
 }
 
-function getEvent(var eventid)
+function getEvent(eventid)
 {
 	var event = {eventName:"Success Through Failures 2",eventLocation:"Charlotte"};
 	
 	return event;
 }
 
-function registerEvent(var user, var eventid)
+function registerEvent(user, eventid)
 {
 	return true;
 }
